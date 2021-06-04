139224d2a2a8f7b522cb2100f30efd5f55178c4e
virustotalop <virustotalop@gmail.com>
2021-06-04 06:04:00 -0700

    Start rewriting language system

---
+++ src/test/java/com/github/virustotalop/obsidianauctions/test/AuctionMessageManagerTest.java
@@ -0,0 +1,14 @@
+package com.github.virustotalop.obsidianauctions.test;
+
+import com.flobi.floauction.message.AuctionMessageManager;
+import org.junit.Test;
+
+public class AuctionMessageManagerTest {
+
+
+    @Test
+    public void testParseMessages() {
+        AuctionMessageManager manager = new AuctionMessageManager();
+    }
+
+}
