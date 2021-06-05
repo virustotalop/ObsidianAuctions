9b69bbbde350f8b05a4b890ad01b7fc91484a62c
virustotalop <virustotalop@gmail.com>
2021-06-05 06:34:02 -0700

    Remove redundant code

--- src/main/java/com/gmail/virustotalop/obsidianauctions/message/AuctionMessageManager.java
+++ src/main/java/com/gmail/virustotalop/obsidianauctions/message/AuctionMessageManager.java
@@ -425,15 +425,14 @@ public class AuctionMessageManager extends MessageManager {
             }
         }
 
+        System.out.println("lore: " + conditionals.get("item-has-lore"));
+
         // Apply replacements and duplicate/remove rows that need it.
         for(int l = 0; l < messageList.size(); l++) {
             String message = ChatColor.translateAlternateColorCodes('&', messageList.get(l));
 
             if(message.length() > 0) {
-                // Remove conditional sections.
-                if(message.length() > 0) {
-                    message = parseConditionals(message, conditionals);
-                }
+                message = parseConditionals(message, conditionals);
             }
             if(message.length() == 0) { //If the length is 0 due to replacements just keep going
                 continue;
@@ -565,7 +564,7 @@ public class AuctionMessageManager extends MessageManager {
                     if(inner.equals("end")) { //If it is end we should just break out
                         inner = "";
                         if(!copy) {
-                            break;
+                            return built;
                         }
                     } else if(inner.startsWith("end-")) { //The end of a conditional should match to the original
                         inner = "";
