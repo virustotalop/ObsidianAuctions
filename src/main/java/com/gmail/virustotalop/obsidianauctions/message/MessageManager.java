e166cf90012608cc3b82112b751be79068541a2d
virustotalop <virustotalop@gmail.com>
2021-06-04 20:23:45 -0700

    Revert "Update MessageManager javadocs on uuid arg"
    This reverts commit 49596821d1040183848ff30a31089bf6b3a48b0c.

--- src/main/java/com/gmail/virustotalop/obsidianauctions/message/MessageManager.java
+++ src/main/java/com/gmail/virustotalop/obsidianauctions/message/MessageManager.java
@@ -4,45 +4,44 @@ import com.gmail.virustotalop.obsidianauctions.auc.Auction;
 import com.gmail.virustotalop.obsidianauctions.auc.AuctionScope;
 
 import java.util.List;
-import java.util.UUID;
 
 public abstract class MessageManager {
 
     /**
      * Sends a message to a player.
      *
-     * @param uuid uuid of the player or null for console
+     * @param playerName username of player or null for console
      * @param auction    the auction being referenced if any
      * @param messageKey message key to be used by floAuction
      */
-    public abstract void sendPlayerMessage(String messageKey, UUID uuid, Auction auction);
+    public abstract void sendPlayerMessage(String messageKey, String playerName, Auction auction);
 
     /**
      * Sends a message to a player.
      *
-     * @param uuid  uuid of the player or null for console
+     * @param playerName  username of player or null for console
      * @param auction     the auction being referenced if any
      * @param messageKeys list of message keys used by floAuction
      */
-    public abstract void sendPlayerMessage(List<String> messageKeys, UUID uuid, Auction auction);
+    public abstract void sendPlayerMessage(List<String> messageKeys, String playerName, Auction auction);
 
     /**
      * Sends a message to a player.
      *
      * @param messageKey   message key used by floAuction
-     * @param uuid   uuid of the player or null for console
+     * @param playerName   username of player or null for console
      * @param auctionScope the auction scope being referenced if any
      */
-    public abstract void sendPlayerMessage(String messageKey, UUID uuid, AuctionScope auctionScope);
+    public abstract void sendPlayerMessage(String messageKey, String playerName, AuctionScope auctionScope);
 
     /**
      * Sends a message to a player.
      *
      * @param messageKeys  list of message keys used by floAuction
-     * @param uuid   uuid of the player or null for console
+     * @param playerName   username of player or null for console
      * @param auctionScope the auction scope being referenced if any
      */
-    public abstract void sendPlayerMessage(List<String> messageKeys, UUID uuid, AuctionScope auctionScope);
+    public abstract void sendPlayerMessage(List<String> messageKeys, String playerName, AuctionScope auctionScope);
 
     /**
      * Sends a message to anyone in the scope of a given auction.
