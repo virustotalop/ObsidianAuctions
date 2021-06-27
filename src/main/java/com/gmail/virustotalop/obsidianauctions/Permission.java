78339d892db3b40bde2b76eee80f0d0c4929fbfc
virustotalop <virustotalop@gmail.com>
2021-06-26 20:05:12 -0700

    Start integration cloud
    This is going to take awhile to translate all the commands but it is at least a start.

---
+++ src/main/java/com/gmail/virustotalop/obsidianauctions/Permission.java
@@ -0,0 +1,10 @@
+package com.gmail.virustotalop.obsidianauctions;
+
+public final class Permission {
+
+    public static final String AUCTION_USE = "auction.use";
+    public static final String AUCTION_START = "auction.start";
+
+    private Permission() { }
+
+}
---
+++ src/main/java/com/gmail/virustotalop/obsidianauctions/command/AuctionCommand.java
@@ -0,0 +1,21 @@
+package com.gmail.virustotalop.obsidianauctions.command;
+
+import cloud.commandframework.annotations.CommandMethod;
+import cloud.commandframework.annotations.CommandPermission;
+import com.gmail.virustotalop.obsidianauctions.Permission;
+import org.bukkit.command.CommandSender;
+
+public class AuctionCommand {
+
+    @CommandMethod("auction|auc")
+    @CommandPermission(Permission.AUCTION_USE)
+    public void auction(CommandSender sender) {
+
+    }
+
+    @CommandMethod("auction|auc start [quantity] [price] [increment] [buynow]")
+    @CommandPermission(Permission.AUCTION_START)
+    public void start(CommandSender sender, Integer quantity, Long price, Long increment, Long buyNow) {
+
+    }
+}
