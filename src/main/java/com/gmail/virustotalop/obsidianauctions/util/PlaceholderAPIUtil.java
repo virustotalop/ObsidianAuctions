fe79f273f1da32f9e733b3649629c517c2060da2
virustotalop <virustotalop@gmail.com>
2021-10-15 20:07:27 -0700

    Use method handle instead of reflection for papi

--- src/main/java/com/gmail/virustotalop/obsidianauctions/util/PlaceholderAPIUtil.java
+++ src/main/java/com/gmail/virustotalop/obsidianauctions/util/PlaceholderAPIUtil.java
@@ -4,22 +4,26 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.ApiStatus;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 
 @ApiStatus.Internal
 public class PlaceholderAPIUtil {
-
-    private static Method setPlaceHolders;
+    
+    private static MethodHandle setPlaceHolders;
 
     public static String setPlaceHolders(Player player, String message) {
         try {
             if(setPlaceHolders == null) {
                 Class<?> placeholderAPI = Class.forName("me.clip.placeholderapi.PlaceholderAPI");
-                setPlaceHolders = placeholderAPI.getDeclaredMethod("setPlaceholders", OfflinePlayer.class, String.class);
-                setPlaceHolders.setAccessible(true);
+                Method reflect = placeholderAPI.getDeclaredMethod("setPlaceholders",
+                        OfflinePlayer.class, String.class);
+                setPlaceHolders = MethodHandles.lookup().unreflect(reflect);
             }
-            return (String) setPlaceHolders.invoke(null, player, message);
-        } catch(Exception e) {
+            return (String) setPlaceHolders.invoke(player, message);
+        }
+        catch(Throwable e) {
             e.printStackTrace();
         }
         return null;
