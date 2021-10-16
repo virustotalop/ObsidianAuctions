bcd450097f6c7a44f92895c011b5196f210c396c
virustotalop <virustotalop@gmail.com>
2021-10-16 01:36:45 -0700

    Fix auction message parser test

--- src/test/java/com/github/virustotalop/obsidianauctions/test/bukkit/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.github.virustotalop.obsidianauctions.test.bukkit;
--- src/test/java/com/github/virustotalop/obsidianauctions/test/message/AuctionMessageParserModule.java
+++ src/test/java/com/github/virustotalop/obsidianauctions/test/message/AuctionMessageParserModule.java
@@ -3,6 +3,8 @@ package com.github.virustotalop.obsidianauctions.test.message;
 import com.github.virustotalop.obsidianauctions.test.message.mock.MockTranslationFactory;
 import com.gmail.virustotalop.obsidianauctions.language.TranslationFactory;
 import com.gmail.virustotalop.obsidianauctions.message.AuctionMessageParser;
+import com.gmail.virustotalop.obsidianauctions.papi.NoImplPapi;
+import com.gmail.virustotalop.obsidianauctions.papi.PlaceholderAPI;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 
@@ -11,5 +13,6 @@ public class AuctionMessageParserModule implements Module {
     public void configure(Binder binder) {
         binder.bind(TranslationFactory.class).to(MockTranslationFactory.class).asEagerSingleton();
         binder.bind(AuctionMessageParser.class).asEagerSingleton();
+        binder.bind(PlaceholderAPI.class).to(NoImplPapi.class).asEagerSingleton();
     }
 }
