a632751efde93c62f6f2ea1339633c3375a8daf2
virustotalop <virustotalop@gmail.com>
2021-10-18 03:08:28 -0700

    Disable NBTCompoundTest for now

--- src/test/java/com/github/virustotalop/obsidianauctions/test/bukkit/NBTCompoundTest.java
+++ src/test/java/com/github/virustotalop/obsidianauctions/test/bukkit/NBTCompoundTest.java
@@ -1,21 +1,16 @@
 package com.github.virustotalop.obsidianauctions.test.bukkit;
 
-import com.gmail.virustotalop.obsidianauctions.nbt.NBTCompound;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.junit.jupiter.api.Test;
 
 public class NBTCompoundTest {
 
     @Test
     public void consumeItem() {
-        /*
-            ItemStack itemStack = new ItemStack(Material.DIRT);
-            ItemMeta itemMeta = itemStack.getItemMeta();
-            itemMeta.setDisplayName("Test");
-            NBTCompound compound = new NBTCompound(itemStack);
-            System.out.println(compound);
-         */
+        /*System.out.println("server: " + this.server);
+        ItemStack itemStack = new ItemStack(Material.DIRT);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName("Test");
+        NBTCompound compound = new NBTCompound(itemStack);
+        System.out.println(compound);*/
     }
 }
