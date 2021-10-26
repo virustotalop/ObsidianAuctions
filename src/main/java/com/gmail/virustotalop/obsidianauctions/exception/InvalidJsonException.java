b22e42071cea09da53f08cded9efcd43b6e4ef5e
virustotalop <virustotalop@gmail.com>
2021-10-26 02:08:15 -0700

    Remove unused InvalidJsonException

--- src/main/java/com/gmail/virustotalop/obsidianauctions/exception/InvalidJsonException.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.virustotalop.obsidianauctions.exception;
-
-public class InvalidJsonException extends Exception {
-
-    public InvalidJsonException(String json) {
-        super("Invalid json '" + json + "'");
-    }
-}
