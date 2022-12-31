3281886d2e19d9082071bb381741ee9013c14904
virustotalop <virustotalop@gmail.com>
2022-12-31 05:20:12 -0800

    Fix sonatype repo url

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ allprojects {
         mavenCentral()
         maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
         maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
-        maven { url = uri("http s://oss.sonatype.org/content/repositories/snapshots") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
         maven { url = uri("https://jitpack.io") }
     }
 }
